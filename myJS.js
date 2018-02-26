884e206af85a81b911196aee48d864e11daf27e8
Hugo-C <hchaste@laposte.net>
2018-02-26 15:36:32 +0100

    Link between two elements is now resizable
    Links are resizing as one of it's elements are moving.
    Links can no longer be duplicate (aka 2 links between two same rects)
    Select an element will now focus it's text automatically

--- myJS.js
+++ myJS.js
@@ -30,7 +30,7 @@ class Element {  // warning : Element need svg to be declared
         this.text.classList.add("foreign"); //to make div fit text
         this.textDiv.classList.add("insideforeign"); //to make div fit text
         //this.textDiv.addEventListener("mousedown", elementMousedown, false);
-        this.text.setAttributeNS(null, "transform", "translate(" + (x + 5) + " " + (y + 10) + ")");
+        this.text.setAttributeNS(null, "transform", "translate(" + (this.x + 5) + " " + (this.y + 10) + ")");
         svg.appendChild(this.text);
         this.text.appendChild(this.textDiv);
         svg.append(this.text);
@@ -40,43 +40,32 @@ class Element {  // warning : Element need svg to be declared
     }
 
     /**
-     * Create a new line between two points
-     * @param x1 {String} : x coordinate of the first point
-     * @param y1 {String} : y coordinate of the first point
-     * @param x2 {String} : x coordinate of the second point
-     * @param y2 {String} : y coordinate of the second point
-     * @return {SVGAElement} the svg line created
-     */
-    static createLine(x1, y1, x2, y2) {
-        let l = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        l.setAttribute("x1", x1);
-        l.setAttribute("y1", y1);
-        l.setAttribute("x2", x2);
-        l.setAttribute("y2", y2);
-        l.setAttribute("stroke", "black");
-        l.setAttribute("stroke-width", STROKE_WIDTH);
-        svg.appendChild(l);
-        return l;
-    }
-
-
-    /**
      * Link two rect with a line
      * @param e1 {Element} : first element to link
      * @param e2 {Element} : second element to link
      */
     static linkRect(e1, e2) {
-        if(e1 !== e2){
-            let pos1 = e1.center;
-            let pos2 = e2.center;
-            let link = Element.createLine(pos1.x, pos1.y, pos2.x, pos2.y);
+        if(!e1.isLinkedTo(e2)){
+            let link = new Link(e1, e2);
             e1.addLink(link);
             e2.addLink(link);
-
             // put elements in front
             e1.putFront();
             e2.putFront();
         }
+    }
+
+    /**
+     * Check if this element and the 'e' element are linked
+     * @param e {Element} : the element to check
+     */
+    isLinkedTo(e) {
+        this.myLinks.forEach(function (link) {
+            if (link.hasElement(e)) {
+                return true;
+            }
+        });
+        return false;
     }
 
     /**
@@ -128,6 +117,7 @@ class Element {  // warning : Element need svg to be declared
      */
     set x(value) {
         this.rect.setAttribute("x", value);
+        this.refreshPosition();
     }
 
     /**
@@ -144,6 +134,7 @@ class Element {  // warning : Element need svg to be declared
      */
     set y(value) {
         this.rect.setAttribute("y", value);
+        this.refreshPosition();
     }
 
     /**
@@ -177,15 +168,84 @@ class Element {  // warning : Element need svg to be declared
      * @param isSelect {boolean}
      */
     set select(isSelect) {
-        isSelect ? this.rect.style.strokeDasharray = '10' : this.rect.style.strokeDasharray = '0';
+        if (isSelect) {
+            this.rect.style.strokeDasharray = '10';
+            rectSelect.textDiv.focus();
+        } else {
+            this.rect.style.strokeDasharray = '0';
+        }
     }
 
     /**
      * Add a link to the element
-     * @param link {SVGAElement} : the link to add
+     * @param link {Link} : the link to add
      */
     addLink(link){
         this.myLinks.push(link);
+    }
+
+    refreshPosition() {
+        this.text.setAttributeNS(null, "transform", "translate(" + (this.x + 5) + " " + (this.y + 10) + ")");
+        this.myLinks.forEach(function (link) {
+           link.refreshPosition();
+        });
+    }
+}
+
+class Link {
+
+    /**
+     * Create a new link between two link
+     * @param e1 {Element} : first element to link
+     * @param e2 {Element} : second element to link
+     */
+    constructor(e1, e2) {
+        let pos1 = e1.center;
+        let pos2 = e2.center;
+        this.line = Link.createLine(pos1.x, pos1.y, pos2.x, pos2.y);
+        this.e1 = e1;
+        this.e2 = e2;
+    }
+
+    /**
+     * Reposition a link
+     */
+    refreshPosition(){
+        let c1 = this.e1.center;
+        let c2 = this.e2.center;
+        this.line.setAttribute("x1", c1.x);
+        this.line.setAttribute("y1", c1.y);
+        this.line.setAttribute("x2", c2.x);
+        this.line.setAttribute("y2", c2.y);
+    }
+
+    /**
+     * Check if the element 'e' is used by this link
+     * @param e {Element}
+     * @returns {boolean}
+     */
+    hasElement(e) {
+        return e === this.e1 || e === this.e2;
+    }
+
+    /**
+     * Create a new line between two points
+     * @param x1 {String} : x coordinate of the first point
+     * @param y1 {String} : y coordinate of the first point
+     * @param x2 {String} : x coordinate of the second point
+     * @param y2 {String} : y coordinate of the second point
+     * @return {SVGAElement} the svg line created
+     */
+    static createLine(x1, y1, x2, y2) {
+        let l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        l.setAttribute("x1", x1);
+        l.setAttribute("y1", y1);
+        l.setAttribute("x2", x2);
+        l.setAttribute("y2", y2);
+        l.setAttribute("stroke", "black");
+        l.setAttribute("stroke-width", STROKE_WIDTH);
+        svg.appendChild(l);
+        return l;
     }
 }
 
