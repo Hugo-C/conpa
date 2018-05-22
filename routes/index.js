44c82de2f3004df188d02644636fad786a028879
Lastshot97 <juju7397@gmail.com>
2018-05-22 10:07:45 +0200

    Adding missing file

--- routes/index.js
+++ routes/index.js
@@ -125,7 +125,8 @@ router.post('/getPlayerDetails', urlencodedParser, function(req, res){
 });
 
 router.post('/getCardGames', urlencodedParser, function(req, res){
-    db.getCardGames(function(err, result){
+    console.log(req.body.tags);
+    db.getCardGamesByTags(JSON.parse(req.body.tags), function(err, result){
         if(err){
             console.log(err);
             res.send('ERROR');
@@ -133,10 +134,25 @@ router.post('/getCardGames', urlencodedParser, function(req, res){
             let cardGames = []; //used to send all card games
             for(let entry in result){
                 let data = {'name': result[entry][keys.CGT_KEY_NAME],
-                            'language': result[entry][keys.CGT_KEY_LANGUAGE]};
+                            'language': result[entry][keys.CGT_KEY_LANGUAGE],
+                            'author': result[entry][keys.CGT_KEY_AUTHOR]};
                 cardGames.push(data);
             }
             res.send(cardGames);
+        }
+    });
+});
+
+router.post('/getAllTags', urlencodedParser, function(req, res){
+    db.getAllTags(function(err, tags){
+        if(err){
+            res.send({'msg': 'ERROR'});
+        }else{
+            data = {'msg': 'OK', 'tags': []};
+            for(let index = 0; index < tags.length; index++){
+                data['tags'].push(tags[index][keys.HTT_KEY_TAG]);
+            }
+            res.send(data);
         }
     });
 });
@@ -156,17 +172,202 @@ router.get('/exportCardGame', function(req, res){
     }
 });
 
+function getCardGameInformation(name, language, callback){
+    let data = {'name': name, 'language': language, 'description': null, 'tags': [], 'allTags': []};
+
+    function retrieveCardgameDescription(){
+        db.getCardGameDescription(name, language, function(err, description){
+            if(err){
+                console.log(err);
+                callback(err);
+            }else{
+                data['description'] = description[0][keys.CGT_KEY_DESCRIPTION];
+                callback(null, data);
+            }
+        });
+    }
+
+    function retrieveCardgameTags(){
+        db.getCardGameTags(name, language, function(err, tags){
+            if(err){
+                console.log(err);
+                callback(err);
+            }else{
+                for(let index = 0; index < tags.length; index++){
+                    data['tags'].push(tags[index][keys.HTT_KEY_TAG]);
+                }
+                retrieveCardgameDescription();
+            }
+        });
+    }
+
+    db.getAllTags(function(err, allTags){
+        if(err){
+            console.log(err);
+            callback(err);
+        }else{
+            for(let index = 0; index < allTags.length; index++){
+                data['allTags'].push(allTags[index][keys.HTT_KEY_TAG]);
+            }
+            retrieveCardgameTags();
+        }
+    });
+}
+
+router.post('/getCardGameInfo', urlencodedParser, function(req, res){
+
+    getCardGameInformation(req.body.name, req.body.language, function(err, data){
+        if(err){
+            console.log(err);
+            res.send({'msg': 'ERROR'});
+        }else{
+            data['msg'] = 'OK';
+            res.send(data);
+        }
+    });
+
+});
+
+router.post('/updateCardgameDescription', urlencodedParser, function(req, res){
+
+    db.updateCardgameDescription(req.body.name, req.body.language, req.body.description, function(err){
+        if(err){
+            console.log(err);
+            res.send('ERROR');
+        }else{
+            res.send('OK');
+        }
+    });
+
+});
+
+router.post('/removeATag', urlencodedParser, function(req, res){
+
+    function sendActualizedTagsList(){
+        data = {'msg': 'OK', 'tags': []};
+        db.getCardGameTags(req.body.name, req.body.language, function(err, tags){
+            if(err){
+                console.log(err);
+                res.send({'msg': 'GET CARDGAME TAGS ERROR'});
+            }else{
+                for(let index = 0; index < tags.length; index++){
+                    data['tags'].push(tags[index][keys.HTT_KEY_TAG]);
+                }
+                res.send(data);
+            }
+        });
+    }
+
+    function removeTagFromCardgameWithId(cardgameId){
+        db.removeATagFromCardgame(cardgameId, req.body.tag, function(err){
+            if(err){
+                console.log(err);
+                res.send({'msg': 'REMOVING TAG ERROR'});
+            }else{
+                sendActualizedTagsList();
+            }
+        });
+    }
+
+    db.getCardGame(req.body.name, req.body.language, function(err, result){
+        if(err){
+            console.log(err);
+            res.send({'msg': 'GET CARDGAME ID ERROR'});
+        }else{
+            removeTagFromCardgameWithId(result[0][keys.CGT_KEY_ID]);
+        }
+    });
+
+});
+
+router.post('/addATag', urlencodedParser, function(req, res){
+
+    function sendActualizedTagsList(){
+        console.log('sending actualized tags list');
+        data = {'msg': 'OK', 'tags': []};
+        db.getCardGameTags(req.body.name, req.body.language, function(err, tags){
+            if(err){
+                console.log(err);
+                res.send({'msg': 'GET CARDGAME TAGS ERROR'});
+            }else{
+                for(let index = 0; index < tags.length; index++){
+                    data['tags'].push(tags[index][keys.HTT_KEY_TAG]);
+                }
+                res.send(data);
+            }
+        });
+    }
+
+    function addTagToCardgameWithId(cardgameId){
+        console.log('adding tags to the cardgame');
+        db.addANewTagToCardgame(cardgameId, req.body.tag, function(err){
+            if(err){
+                console.log(err);
+                res.send({'msg': 'ADDING NEW TAG TO CARDGAME ERROR'});
+            }else{
+                sendActualizedTagsList();
+            }
+        });
+    }
+
+    function searchCardgameId(){
+        console.log('retrieving cardgame id');
+        db.getCardGame(req.body.name, req.body.language, function(err, result){
+            if(err){
+                console.log(err);
+                res.send({'msg': 'GET CARDGAME ID ERROR'});
+            }else{
+                addTagToCardgameWithId(result[0][keys.CGT_KEY_ID]);
+            }
+        })
+    }
+
+    function addANewTag(){
+        console.log('adding a new tag to the tags table');
+        db.addANewTag(req.body.tag, function(err){
+            if(err){
+                console.log(err);
+                res.send({'msg': 'ADDING NEW TAG ERROR'});
+            }else{
+                searchCardgameId();
+            }
+        })
+    }
+
+    db.existsTag(req.body.tag, function(err, exists){
+        console.log('checking if the tag already exists');
+        if(err){
+            console.log(err);
+            res.send({'msg': 'CHECK IF TAG EXISTS ERROR'});
+        }else{
+            if(exists){
+                searchCardgameId();
+            }else{
+                addANewTag();
+            }
+        }
+    });
+
+});
+
 router.post('/updateCardGame', urlencodedParser, function(req, res){
     let update = req.body.update;
     let cardGamePath = "./upload/" + req.body.csv;
     if(update === 'yes'){
         console.log("overwrite data");
-        importCSV.importFromCsv(cardGamePath, false);
-        res.send('OK');
+        importCSV.importFromCsv(req.body.name, req.body.language, req.body.author, cardGamePath, false);
+        getCardGameInformation(req.body.name, req.body.language, function(err, data){
+            if(err){
+                res.send({'send': 'ERROR'});
+            }else{
+                data['send'] = 'OK';
+                res.send(data);
+            }
+        });
     }else{
         console.log("no update !");
         fs.unlinkSync(cardGamePath);
-        res.send('NO UPDATE');
+        res.send({'send': 'NO UPDATE'});
     }
 });
 
@@ -176,21 +377,10 @@ router.post('/updateCardGame', urlencodedParser, function(req, res){
  * @param {string} csvPath : path to the csv on the server
  * @param {callback} cardGameExistsCallback : used to return the answer (takes a boolean param and file content)
  */
-function checkIfCardGameExists(csvPath, cardGameExistsCallback){
+function checkIfCardGameExists(name, language, cardGameExistsCallback){
     console.log('checking if card game exists');
-    fs.readFile(csvPath, function(err, data) {
-
-        try{
-            var records = parse(data, {columns: true}); // data of the file
-            //console.log(records);
-            if(records.length > 0){ // file is not empty
-                db.cardGameExists(records[0]['nom jeu'], records[0]['langue'], function(exists){
-                    cardGameExistsCallback(null, exists, records);
-                });
-            }
-        }catch(error){
-            cardGameExistsCallback(error);
-        }
+    db.cardGameExists(name, language, function(exists){
+        cardGameExistsCallback(null, exists);
     });
 }
 
@@ -205,34 +395,57 @@ function generateRandomCsvFileName(){
 }
 
 router.post('/importCardGame', function (req, res) {
+    let params = querystring.parse(url.parse(req.url).query);
+
+    function processFile(fileName){
+        importCSV.importFromCsv(params['name'], params['language'], params['author'], "./upload/" + fileName, true);
+        getCardGameInformation(req.body.name, req.body.language, function(err, data){
+            if(err){
+                res.send({'msg': 'ERROR'});
+            }else{
+                data['msg'] = 'OK';
+                res.send(data);
+            }
+        });
+    }
+
+    function checkIfCardGameExists(name, language, fileName){
+        console.log('checking if card game exists');
+        db.getCardGame(name, language, function(err, result){
+              if(err){
+                  console.log(err);
+                  res.send({'msg': 'ERROR'});
+              }else if(result.length == 1 && result[0][keys.CGT_KEY_AUTHOR] == params['author']){
+                  res.send({'msg': 'UPDATE?', 'file': fileName});
+              }else if(result.length == 0){
+                  processFile(fileName);
+              }else{
+                  res.send({'msg': 'UNAUTHORIZED'});
+              }
+        });
+    }
+
+    function storeCsvFileOnServer(){
+        // Save file on the server
+        let file = req.file; // file content and metadata
+        let fileName = generateRandomCsvFileName();
+
+        fs.writeFile("./upload/" + fileName, file.buffer, function(err){
+            if(err){
+                console.log(err);
+                res.send({'msg': 'ERROR'});
+            }else{
+                checkIfCardGameExists(params['name'], params['language'], fileName);
+            }
+        });
+    }
+
     upload(req, res, function (err) {
         if (err) {
             console.log(err);
-            res.send('ERROR');
+            res.send({'msg': 'ERROR'});
         }else{
-            // Save file on the server
-            let file = req.file; // file content and metadata
-            var fileName = generateRandomCsvFileName();
-            var dest = "./upload/" + fileName; // path to the file (uploaded files are saved on the upload folder)
-            fs.writeFile(dest, file.buffer, function(err){
-                if(err){
-                    console.log(err);
-                    res.send('ERROR');
-                }
-            });
-            checkIfCardGameExists(dest, function(err, exists, cardGame){
-                if(err){
-                    res.send('ERROR');
-                }else{
-                    console.log('card game exists ? : ' + exists);
-                    if(exists){
-                        res.send(fileName);
-                    }else{
-                        importCSV.importFromCsv(dest, true);
-                        res.send('OK');
-                    }
-                }
-            });
+            storeCsvFileOnServer();
         }
     });
 });
